7ea55910661f6bab66e5f7e47f97c77da9b99cf2
wlm <william.malmgren.hanning@parkingaid.se>
2026-02-26 16:24:41 +0100

    Update to config

--- lua/custom/plugins/autoformat.lua
+++ lua/custom/plugins/autoformat.lua
@@ -6,7 +6,15 @@ vim.opt.colorcolumn = '80'
 local reloading = false
 
 local function to_tabs()
-  if vim.bo.buftype ~= '' or reloading then return end
+  if vim.bo.buftype ~= '' or reloading then
+    return
+  end
+  if vim.bo.filetype == 'python' then
+    return
+  end -- skip Python
+  if vim.bo.filetype == 'json' then
+    return
+  end -- skip json
   for _, l in ipairs(vim.api.nvim_buf_get_lines(0, 0, 50, false)) do
     local s = l:match '^( +)[^%s]'
     if s then
@@ -24,11 +32,33 @@ vim.api.nvim_create_autocmd('BufReadPost', { pattern = '*', callback = to_tabs }
 vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = '*',
   callback = function()
-    if vim.bo.buftype ~= '' then return end
+    if vim.bo.buftype ~= '' then
+      return
+    end
     reloading = true
-    vim.cmd 'silent! edit'  -- reload formatter's output from disk
+    vim.cmd 'silent! edit' -- reload formatter's output from disk
     reloading = false
     to_tabs()
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.bo.expandtab = true
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'json',
+  callback = function()
+    vim.bo.expandtab = true
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
   end,
 })
 
